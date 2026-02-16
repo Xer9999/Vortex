e6388c5ec163ea8d2edf192e43b43ea941c09c7f
Vortex <sanfordiscord@gmail.com>
2026-02-17 03:43:46 +0700

    Implement game ID checker in vortex.lua
    Added a game ID checker to ensure script runs only in specific games.

--- vortex.lua
+++ vortex.lua
@@ -1,1 +1,17 @@
+-- Game ID Checker Script
+local GAME_ID_1 = 13822562292
+local GAME_ID_2 = 15288629078
 
+local function getCurrentGameId()
+    return game.GameId
+end
+
+local currentGameId = getCurrentGameId()
+
+if currentGameId == GAME_ID_1 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Xer9999/Vortex/refs/heads/main/Midnight_Chasers.lua"))()
+elseif currentGameId == GAME_ID_2 then
+    print("test") 
+else
+    game.Players.LocalPlayer:Kick("This script can only be used in specific games.")
+end
