eba6a0067a45cd6fa1b4e5016e2ba85e9c819b03
Vortex <sanfordiscord@gmail.com>
2026-02-17 21:38:14 +0700

    Load script for GAME_ID_2 instead of printing test

--- vortex.lua
+++ vortex.lua
@@ -11,7 +11,7 @@ local currentGameId = getCurrentGameId()
 if currentGameId == GAME_ID_1 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Xer9999/Vortex/refs/heads/main/Midnight_Chasers.lua"))()
 elseif currentGameId == GAME_ID_2 then
-    print("test") 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Xer9999/Vortex/refs/heads/main/Highway%20Legends.lua"))()
 else
     game.Players.LocalPlayer:Kick("This script can only be used in specific games.")
 end
