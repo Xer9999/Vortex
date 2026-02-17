416e0dd22060308e1efdfa68ed952bfa0930e23c
Vortex <sanfordiscord@gmail.com>
2026-02-17 21:42:17 +0700

    Update place ID checks

--- vortex.lua
+++ vortex.lua
@@ -1,16 +1,11 @@
--- Game ID Checker Script
-local GAME_ID_1 = 13822562292
-local GAME_ID_2 = 15288629078
+local PLACE_ID_1 = 13822562292
+local PLACE_ID_2 = 15288629078
 
-local function getCurrentGameId()
-    return game.GameId
-end
+local currentId = game.PlaceId
 
-local currentGameId = getCurrentGameId()
-
-if currentGameId == GAME_ID_1 then
+if currentId == PLACE_ID_1 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Xer9999/Vortex/refs/heads/main/Midnight_Chasers.lua"))()
-elseif currentGameId == GAME_ID_2 then
+elseif currentId == PLACE_ID_2 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Xer9999/Vortex/refs/heads/main/Highway%20Legends.lua"))()
 else
     game.Players.LocalPlayer:Kick("This script can only be used in specific games.")
